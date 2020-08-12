77d698b657128d212bbfd054c022fe5f0ab1d73a
narirock <nari9rock@hotmail.com>
2020-08-12 14:49:51 -0300

    first commit

---
+++ resources/js/Pages/Homepage/index.js
@@ -0,0 +1,12 @@
+import { InertiaApp } from '@inertiajs/inertia-react'
+import React from 'react'
+import { render } from 'react-dom'
+
+export default function Homepage(){
+    return(
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    )
+}
+
